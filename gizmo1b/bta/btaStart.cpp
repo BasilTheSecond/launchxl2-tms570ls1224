d264ca0397e53f6abb22e1cb98c55b79cf55f135
Dragan Ostojic <dragan.ostojic@outlook.com>
2018-03-06 22:51:02 -0500

    LibSci

--- gizmo1b/bta/btaStart.cpp
+++ gizmo1b/bta/btaStart.cpp
@@ -1,25 +1,12 @@
 #include "FreeRTOS.h"
-#include "os_task.h"
 #include "btaStart.h"
 #include "btaApp.h"
 
-static const TickType_t k_btaInitTaskDelay = pdMS_TO_TICKS(1000);
-
-static void btaInitTask(void* parameter)
+void btaStart()
 {
     BtaApp btaApp("btaApp");
     btaApp.start();
-    while (1) {
-        vTaskDelay(k_btaInitTaskDelay);
-
-    }
-}
-
-void btaStart()
-{
-    xTaskCreate(btaInitTask, "btaInit", configMINIMAL_STACK_SIZE, NULL,
-                                                        tskIDLE_PRIORITY, NULL);
     vTaskStartScheduler();
-    while (1) {
+    while (true) {
     }
 }
--- gizmo1b/library/libAppStart.h
+++
@@ -1,7 +0,0 @@
-#ifndef _LIB_APP_START_H_
-#define _LIB_APP_START_H_
-
-#include "btaStart.h"
-#define libAppStart btaStart
-
-#endif /* _LIB_APP_START_H_ */
